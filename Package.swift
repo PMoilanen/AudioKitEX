f3014e68c9d6ea9b4403b22a16b9b4b1393a61bd
Paavo Moilanen <paavo.moilanen@tuup.fi>
2022-03-04 16:26:38 +0200

    Trying to get the pure playgrounds to work with SPM settings

--- Package.swift
+++ Package.swift
@@ -5,12 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "AudioKitEX",
-    platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
-    products: [.library(name: "AudioKitEX", targets: ["AudioKitEX"])],
-    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", from: "5.3.0")],
+    platforms: [.macOS(.v11), .iOS(.v11), .tvOS(.v11)],
+    products: [
+        .library(name: "AudioKitEX", targets: ["AudioKitEX"])
+     ],
+    dependencies: [
+        .package(url: "https://github.com/PMoilanen/AudioKit", from: "5.3.0"),
+        .package(url: "https://github.com/PMoilanen/CAudioKitEX", from: "5.3.0"),
+        .package(url: "https://github.com/PMoilanen/AudioKitUI", from: "0.1.0")
+    ],
     targets: [
-        .target(name: "AudioKitEX", dependencies: ["AudioKit", "CAudioKitEX"]),
-        .target(name: "CAudioKitEX", cxxSettings: [.headerSearchPath(".")]),
+        .target(name: "AudioKitEX", dependencies: ["AudioKit","CAudioKitEX"]),
+//        .target(name: "CAudioKitEX", cxxSettings: [.headerSearchPath(".")]),
         .testTarget(name: "AudioKitEXTests", dependencies: ["AudioKitEX"], resources: [.copy("TestResources/")])
     ],
     cxxLanguageStandard: .cxx14
